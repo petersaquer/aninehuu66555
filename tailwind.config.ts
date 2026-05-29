889826e310236d14551e6d517b2eef123f0811f1
petersaquer <servernogardpy@gmail.com>
2026-05-29 02:56:46 -0300

    fix: Update Tailwind config with client/index.html content path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{ts,tsx,jsx,js}'],
+  content: ['./src/**/*.{ts,tsx,jsx,js}', './index.html'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Sora', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
+      },
+      fontSize: {
+        sm: '0.875rem',
       },
     },
   },
